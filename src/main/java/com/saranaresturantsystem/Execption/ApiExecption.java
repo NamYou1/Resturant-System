c1e568173326d09262abc1f3263f87791400d001
NamYo <namyou854@gmail.com>
2026-03-31 21:52:00 +0700

    featur(category): crud , pagination , search mutiple field , customer apiResponse and execption

--- src/main/java/com/saranaresturantsystem/Execption/ApiExecption.java
+++ src/main/java/com/saranaresturantsystem/Execption/ApiExecption.java
@@ -1,12 +1,18 @@
 package com.saranaresturantsystem.Execption;
+
 import lombok.*;
 import org.springframework.http.HttpStatus;
+
 @Getter
 @Setter
-//@AllArgsConstructor
-//@NoArgsConstructor
-@RequiredArgsConstructor
-public class ApiExecption extends  RuntimeException{
-    private final HttpStatus status ;
-    private final String message ;
+@AllArgsConstructor
+public class ApiExecption extends RuntimeException {
+    private final HttpStatus status;
+    private final String message;
+
+    public ApiExecption(String message, HttpStatus status) {
+        super(message);
+        this.message = message;
+        this.status = status;
+    }
 }
--- src/main/java/com/saranaresturantsystem/Mappers/UserMapper.java
+++
@@ -1,4 +0,0 @@
-package com.saranaresturantsystem.Mappers;
-///  use for convert dto to  user simple and convert model to dto
-public class UserMapper {
-}
