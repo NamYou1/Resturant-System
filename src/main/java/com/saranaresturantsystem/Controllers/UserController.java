c953d200799a23472707d4289e3aa6e0bb803816
NamYo <namyou854@gmail.com>
2026-04-02 21:17:19 +0700

    feature(UniqueChecker) validation duplicationResource Execption and validation on CategoryCreate , update

--- src/main/java/com/saranaresturantsystem/Controllers/UserController.java
+++
@@ -1,7 +0,0 @@
-package com.saranaresturantsystem.Controllers;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class UserController {
-}
--- src/main/java/com/saranaresturantsystem/Repositories/UserRepositories.java
+++
@@ -1,7 +0,0 @@
-package com.saranaresturantsystem.Repositories;
-
-import liquibase.license.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepositories extends JpaRepository<User , Long> {
-}
